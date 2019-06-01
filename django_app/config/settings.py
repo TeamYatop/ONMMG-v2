9f36317cabee178f37bc06977c15e420e1f43efb
Hungjoon Kim <salmonjoon@gmail.com>
2019-06-01 17:16:30 +0900

    Fix misspellings

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            TEMPLATE_DIR
+            TEMPLATES_DIR,
         ],
         'APP_DIRS': True,
         'OPTIONS': {
